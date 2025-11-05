8ef198be978b46b73a189ffdfcff06166bf20bfe
shivarajshanthaiah <shivarajshanthaiah@gmai.com>
2025-11-05 13:59:52 +0530

    added uber-zap for logging

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,19 @@
 package main
 
-import "github.com/shivarajshanthaiah/todo-app/internal/boot"
+import (
+	"log"
+
+	"github.com/shivarajshanthaiah/todo-app/internal/boot"
+)
 
 func main() {
 
-	boot.Setup()
-	server := boot.NewServer()
-	server.StartServer(boot.Cnfg.SERVERPORT)
+	cnfg, db, redis, logger, err := boot.Setup()
+	if err != nil {
+		log.Fatalf("failed to initialize dependencies: %v", err)
+	}
+	server := boot.NewServer(db, redis, logger, cnfg)
+	if err := server.StartServer(cnfg.SERVERPORT); err != nil {
+		log.Fatalf("server exited with error: %v", err)
+	}
 }
